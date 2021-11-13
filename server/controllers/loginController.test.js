f0d95dfe09fbd4c212cdcd0d1a96d715b6dddd2b
David <david.llobet6@gmail.com>
2021-11-13 18:56:29 +0100

    Create and check tests for userSignUp function

--- server/controllers/loginController.test.js
+++ server/controllers/loginController.test.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const User = require("../../database/models/user");
-const { userLogin } = require("./loginController");
+const { userLogin, userSignUp } = require("./loginController");
 
 jest.mock("../../database/models/user");
 jest.mock("bcrypt");
@@ -58,6 +58,59 @@ describe("Given a userLogin function", () => {
 
       expect(next).toHaveBeenCalledWith(error);
       expect(next.mock.calls[0][0]).toHaveProperty("code", error.code);
+    });
+  });
+  // describe
+  // test
+});
+
+describe("Given a userSignUp function", () => {
+  describe("When it receives a request with an existing username", () => {
+    test("Then it should invoke the next function with an error", async () => {
+      const usernameTest = "Rodica";
+
+      const req = {
+        body: {
+          username: usernameTest,
+        },
+      };
+
+      const res = {};
+
+      User.findOne = jest.fn().mockResolvedValue(true);
+      const error = new Error("Username already taken");
+      error.code = 400;
+      const next = jest.fn();
+
+      await userSignUp(req, res, next);
+
+      expect(User.findOne).toHaveBeenCalled();
+      expect(next).toHaveBeenCalledWith(error);
+      expect(next.mock.calls[0][0]).toHaveProperty("code", error.code);
+    });
+  });
+  describe("When it receives a request with a new username", () => {
+    test("Then it should respond with the new user", async () => {
+      const userTest = {
+        name: "Xavi",
+        username: "tadeo",
+        password: "Aixoesmoltdificil",
+      };
+
+      const req = {
+        body: userTest,
+      };
+
+      const res = {
+        json: jest.fn(),
+      };
+
+      User.findOne = jest.fn().mockResolvedValue(false);
+      User.create = jest.fn().mockResolvedValue(userTest);
+
+      await userSignUp(req, res);
+
+      expect(res.json).toHaveBeenCalledWith(userTest);
     });
   });
 });
