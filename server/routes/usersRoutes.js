d2b2389580ed85a00d318a1633ceaaf7ee6c0fb5
David <david.llobet6@gmail.com>
2021-11-13 20:31:39 +0100

    Create getAllUsers controller

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { userSignUp, userLogin } = require("../controllers/loginController");
+const { getAllUsers } = require("../controllers/usersController");
 
 const router = express.Router();
 
 router.post("/register", userSignUp);
 router.post("/login", userLogin);
+router.get("/users", getAllUsers);
 
 module.exports = router;
