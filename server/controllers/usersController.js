d02ae6cc2c4d76dfe72badf2ebc3b7a9e6d4c0c0
David <david.llobet6@gmail.com>
2021-11-15 20:38:59 +0100

    Create userSignUp controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,6 @@
+const chalk = require("chalk");
+const debug = require("debug")("user:controller");
+const bcrypt = require("bcrypt");
 const User = require("../../database/models/user");
 
 const getAllUsers = async (req, res, next) => {
@@ -11,6 +14,28 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
+const userSignUp = async (req, res, next) => {
+  const newUser = req.body;
+  const user = await User.findOne({ username: newUser.username });
+  if (user) {
+    debug(chalk.redBright("Username already taken"));
+    const error = new Error("Username already taken");
+    error.code = 400;
+    next(error);
+  } else {
+    newUser.name = newUser.name ? newUser.name : "";
+    newUser.friends = [];
+    newUser.enemies = [];
+    newUser.image = newUser.image ? newUser.image : "";
+    newUser.imageLocal = newUser.imageLocal ? newUser.imageLocal : "";
+    newUser.bio = newUser.bio ? newUser.bio : "";
+    newUser.password = await bcrypt.hash(newUser.password, 10);
+    User.create(newUser);
+    res.json().status(200);
+  }
+};
+
 module.exports = {
   getAllUsers,
+  userSignUp,
 };
